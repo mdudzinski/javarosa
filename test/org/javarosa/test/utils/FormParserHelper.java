0a8ca7ff8209f035a2aa9cbdd36949ac6753497e
Michal Dudzinski <mdudzinski@soldevelo.com>
2017-11-09 16:17:20 +0100

    Make test forms parsing code reusable

---
+++ test/org/javarosa/test/utils/FormParserHelper.java
@@ -0,0 +1,47 @@
+package org.javarosa.test.utils;
+
+import org.javarosa.core.model.FormDef;
+import org.javarosa.xform.parse.XFormParser;
+import org.javarosa.xform.parse.XFormParserReporter;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+
+public final class FormParserHelper {
+
+    private FormParserHelper() {
+    }
+
+    public static ParseResult parse(Path formName) throws IOException {
+        XFormParser parser = new XFormParser(new FileReader(formName.toString()));
+        final List<String> errorMessages = new ArrayList<>();
+        parser.attachReporter(new XFormParserReporter() {
+            @Override
+            public void warning(String type, String message, String xmlLocation) {
+                errorMessages.add(message);
+                super.warning(type, message, xmlLocation);
+            }
+
+            @Override
+            public void error(String message) {
+                errorMessages.add(message);
+                super.error(message);
+            }
+        });
+
+        return new ParseResult(parser.parse(), errorMessages);
+    }
+
+    public static class ParseResult {
+        public final FormDef formDef;
+        public final List<String> errorMessages;
+
+        ParseResult(FormDef formDef, List<String> errorMessages) {
+            this.formDef = formDef;
+            this.errorMessages = errorMessages;
+        }
+    }
+}
